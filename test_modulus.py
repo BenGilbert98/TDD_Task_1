085a68234cf12afb3661f281f5899fe936001d16
BenGilbert98 <73166552+BenGilbert98@users.noreply.github.com>
2020-11-10 14:28:09 +0000

    more detailed pseudo code for test_modulus file

--- test_modulus.py
+++ test_modulus.py
@@ -1,14 +1,15 @@
 from modulus import Modulus
 import unittest
-import pytest
 
 
 class Modulus_test(unittest.TestCase):
     mod = Modulus()
 
     def test_modulus(self):
-        self.assertEqual(self.mod.modulus(2, 2), 0)
+        self.assertEqual(self.mod.modulus(4, 2), 0)
+        # checks to see if there is no remainder when 4 is divided by 2
 
     # test_positive function defined
     def test_positive(self):
-        self.assertEqual(self.mod.is_positive(2, 2), True)
+        self.assertEqual(self.mod.is_positive(2, 4), True)
+        # checks to see if 2 and 4 are positive
